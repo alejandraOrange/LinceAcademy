0a2e4fbe73fec52b427d6aa4fea3b610c9a1f3d1
alejandraOrange <alejandraOrange>
2025-04-14 10:59:10 -0300

    ap2 concluida

--- FormacaoDart/ap4_Operadores/ap2.dart
+++ FormacaoDart/ap4_Operadores/ap2.dart
@@ -1,5 +1,16 @@
 void main() {
-  List<String> entrada = ["5O0", "8", "JOJ0", "11"];
+  List<String> entrada = [
+    "10",
+    "2XXL7",
+    "JOJ0",
+    "99",
+    "381",
+    "AD44",
+    "47",
+    "2B",
+    "123",
+    "78",
+  ];
   // Mapeia cada item tentando converter para int, caso contrário retorna 0
   List<int> convertida =
       entrada.map((item) {
